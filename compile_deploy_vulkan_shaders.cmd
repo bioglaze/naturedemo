d09cd0d06378cdff03338d2c9baae3ae5b4ada84
Timo Wiren <timo.wiren@gmail.com>
2019-06-13 19:15:03 +0300

    Update shader compile script

--- compile_deploy_vulkan_shaders.cmd
+++ compile_deploy_vulkan_shaders.cmd
@@ -3,5 +3,7 @@ md build
 %VULKAN_SDK%/bin/glslangValidator -D -V -S frag -e mainFS src/water.hlsl -o build/water_fs.spv
 %VULKAN_SDK%/bin/glslangValidator -D -V -S vert -e mainVS src/sky.hlsl -o build/sky_vs.spv
 %VULKAN_SDK%/bin/glslangValidator -D -V -S frag -e mainFS src/sky.hlsl -o build/sky_fs.spv
+%VULKAN_SDK%/bin/glslangValidator -D -V -S vert -e mainVS src/ground.hlsl -o build/ground_vs.spv
+%VULKAN_SDK%/bin/glslangValidator -D -V -S frag -e mainFS src/ground.hlsl -o build/ground_fs.spv
 pause
 
